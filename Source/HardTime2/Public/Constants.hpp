599fe5a14356c0d2796d1a75bb1207bbb8dcd0bf
Thomas Wallace <trihardar@gmail.com>
2018-11-27 23:17:43 +0000

    Diagnosed the memory corruption! Constants file given temporary fix, with note added for future fix

--- Source/HardTime2/Public/Constants.hpp
+++ Source/HardTime2/Public/Constants.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr int c_playerCount = 1;
+constexpr int c_playerCount = 16;  //This MUST match the number of playerData registered in world. Or memory corruption occurs. Correct this.
 constexpr double c_aggro_scale = 30;
 constexpr double c_aggroCooldown = 0.15;
 constexpr bool c_debug = true;
