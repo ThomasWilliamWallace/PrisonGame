3210990286303c213d2c15c3eb155066d070eda0
Thomas Wallace <trihardar@gmail.com>
2020-09-09 22:26:16 +0100

    Use pLog from htn_planner project.

--- Source/HardTime2/External_includes.cpp
+++ Source/HardTime2/External_includes.cpp
@@ -2,3 +2,4 @@
 // Hack to allow including a subset of the htn_planner files into Unreal: https://answers.unrealengine.com/questions/894331/how-do-you-includeexclude-files-from-build-1.html
 
 #include "../htn_planner/htn_planner/Private/Constants.cpp"
+#include "../htn_planner/htn_planner/Private/pLog.cpp"
