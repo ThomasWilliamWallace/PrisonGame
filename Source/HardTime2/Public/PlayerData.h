c3e62e639109f3595187ef052ca1c13d2d506fca
Thomas Wallace <trihardar@gmail.com>
2020-09-13 13:15:29 +0100

    Syntax

--- Source/HardTime2/Public/PlayerData.h
+++ Source/HardTime2/Public/PlayerData.h
@@ -57,7 +57,7 @@ public:
 
 	virtual class UWorld* GetWorld() const override;
 
-	std::string CharacterName()
+	std::string CharacterName();
 	AbstractPlayerData abstractPlayerData = AbstractPlayerData();
 };
 
