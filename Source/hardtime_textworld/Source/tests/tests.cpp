f3a35f2227672401172642d2fc2a2e2368198cad
Thomas Wallace <trihardar@gmail.com>
2021-08-07 21:55:38 +0100

    Intellisense uses the HTN_TEXT_ONLY macro. First draft test of htn planner added.

--- Source/hardtime_textworld/Source/tests/tests.cpp
+++ Source/hardtime_textworld/Source/tests/tests.cpp
@@ -1,14 +1,92 @@
 #include "gtest/gtest.h"
+#include "TranslateToHTNWorldState.h"
+#include "HTNPlanner.h"
+#include "HTNDomain.h"
+#include "Locations.h"
+#include "SimWorld.h"
+#include "EItemType.h"
+#include "pLog.h"
+#include "PlayerData.h"
+#include "Missions.h"
+#include "BasicAI.h"
+#include "HTNPrimitives.h"
+
+//#include "Constants.h"
+//#include "Actions.h"
+//#include "HTNPlanner.h"
+//#include "PlayerRegistry.h"
+//#include "Names.h"
 
 
-namespace {
+namespace tests {
 	TEST(SimpleTest, GreaterThan1) {
 		EXPECT_GT(2, 1);
 	}
 
-	// Tests factorial of 0.
 	TEST(SimpleTest, EqualTo1) {
-		EXPECT_EQ(1, 0);
+		EXPECT_EQ(1, 1);
+	}
+
+	TEST(HTNPlannerIterative, EqualTo1) {
+        // Setup
+        USimWorld world;
+        world.items.push_back(new Item(EItemType::extinguisher, ELocations::mainHall));
+        world.items.push_back(new Item(EItemType::bottle, ELocations::mainHall));
+        world.items.push_back(new Item(EItemType::hammer, ELocations::circuitTrack));
+        world.items.push_back(new Item(EItemType::ball, ELocations::mainHall));
+        Item* ball_in_hall = world.items[3];
+        world.items.push_back(new Item(EItemType::mirror, ELocations::bedroom));
+        world.items.push_back(new Item(EItemType::rock, ELocations::library));
+
+        UPlayerRegistry& playerRegistry = world.playerRegistry;
+        PlayerMap& playerMap = playerRegistry.m_playerMap;
+
+        //Add players
+        UPlayerData* player;
+
+
+        player = new UPlayerData();
+        
+        player->m_playerName = "Thomas";
+        player->aiController = AIController(AI::htnAI);
+        player->missionClass = std::make_shared<MissionClass>(player);
+        world.playerRegistry.RegisterPlayer(player);
+
+        player = new UPlayerData();
+        player->m_playerName = "Jack";
+        player->aiController = AIController(AI::humanAI);
+        player->missionClass = std::make_shared<MissionClass>(player);
+        world.playerRegistry.RegisterPlayer(player);
+
+        player = new UPlayerData();
+        player->m_playerName = "Rupert";
+        player->aiController = AIController(AI::htnAI);
+        player->missionClass = std::make_shared<MissionClass>(player);
+        world.playerRegistry.RegisterPlayer(player);
+
+        //print initial missions
+        for (auto playerIter = playerMap.cbegin(); playerIter != playerMap.cend(); ++playerIter)
+        {
+            UPlayerData* player = dynamic_cast<UPlayerData*>(playerIter->second);
+            pLog(player->missionClass->MissionNarrative(), true);
+        }
+
+		//update worldstate from real world
+		HTNWorldState htnWorldState = TranslateToHTNWorldState(player, world, world.playerRegistry.m_playerMap, nullptr);
+		//make new plan
+		HTNWorldState htnWorldStateDFSCopy(htnWorldState);
+		HTNCompound* missionPtr = new PrisonerBehaviourCompound();
+
+		HTNPrimitiveList htnPlan = HTNIterative(htnWorldStateDFSCopy, *missionPtr, 0);
+
+        // Check resulting plan
+        EXPECT_EQ(size(htnPlan), 3);
+        EXPECT_EQ(htnPlan[0]->m_name, "PickUpItemByPtrPrim");
+        HTNPrimitivePtr action_1 = htnPlan[0];
+        std::shared_ptr<PickUpItemByPtrPrim> pickUpItemByPtrPrim = std::static_pointer_cast<PickUpItemByPtrPrim>(action_1);
+        EXPECT_EQ(pickUpItemByPtrPrim->m_itemFocus->m_realItem, ball_in_hall);
+        EXPECT_EQ(htnPlan[1]->m_name, "GoToGymPrim");
+        EXPECT_EQ(htnPlan[2]->m_name, "DropItemPrim");
 	}
 
 }  // namespace
