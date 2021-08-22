50058f67d891c935fe68617ea4357e06bc06fefc
Thomas Wallace <trihardar@gmail.com>
2021-08-22 17:35:02 +0100

    Split htn_planner back out as subrepo.

--- Source/hardtime_textworld/Public/pLog.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <sstream>
-
-void pLog(std::string logText, bool forcePrint = false);
-void pLog(std::string logText, std::string logText2, bool forcePrint = false);
-void pLog(std::stringstream& logStream, bool forcePrint = false);
