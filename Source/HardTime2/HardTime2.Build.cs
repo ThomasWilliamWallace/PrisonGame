7869d7405279f0203d59c07baabe71fa6cf47967
Thomas Wallace <trihardar@gmail.com>
2021-08-29 14:23:41 +0100

    Add htn_planner into unreal build files.

--- Source/HardTime2/HardTime2.Build.cs
+++ Source/HardTime2/HardTime2.Build.cs
@@ -9,8 +9,12 @@ public class HardTime2 : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "AIModule" });
+		// TODO it's not clear if we need all these lines. It may be best to either add the htn_planner module, or include it's sources. But probably not both.
+		// Then revisit bUseRTTI as well.
+		PublicDependencyModuleNames.Add("htn_planner");
+		bUseRTTI = true;
 
-		//PublicIncludePaths.Add("C:/Users/triha/OneDrive/Documents/Unreal Projects/HardTime2 4.26/Source/imports");
-		//PublicIncludePaths.Add("C:/Users/triha/OneDrive/Documents/Unreal Projects/HardTime2 4.26/Source/htn_planner/htn_planner/Public");
+		PublicIncludePaths.Add("C:/Users/triha/OneDrive/Documents/Unreal Projects/HardTime2 4.26/Source/htn_planner/Public");
+		PublicIncludePaths.Add("C:/Users/triha/OneDrive/Documents/Unreal Projects/HardTime2 4.26/Source/htn_planner/Private");
 	}
 }
